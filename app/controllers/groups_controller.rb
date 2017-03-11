6dd2d629590245a7a972a9c79fb4f9ed8b86b63f
wangdonghui <wangdonghuiibt@gmail.com>
2017-03-11 14:09:15 +0800

    when created a group, join it automatically

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -20,6 +20,7 @@ class GroupsController < ApplicationController
     @group.user = current_user
 
     if @group.save
+      current_user.join!(@group)
       redirect_to groups_path
     else
       render :new
@@ -66,7 +67,7 @@ class GroupsController < ApplicationController
       flash[:warning] = "You're not a member yet, how can I quit,XD?"
     end
     redirect_to group_path(@group)
-    
+
   end
 
   private
